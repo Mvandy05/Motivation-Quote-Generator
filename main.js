e5951574a3f35ce090f8b40a669ea4352e14fbea
Matt Vanderhoef <43138880+Mvandy05@users.noreply.github.com>
2023-09-20 15:35:42 -0400

    Add Factory Function and Quotes

---
+++ main.js
@@ -0,0 +1,36 @@
+//Array of newQuotes
+const quotesArr = [];
+
+//newQuote Factory Function
+const newQuote = (part1, part2, part3, authorFirstName, authorLastName) => {
+    quotesArr.push({    
+    part1,
+    part2,
+    part3,
+    authorFirstName,
+    authorLastName
+    });
+};
+
+
+
+
+/*
+newQuote(`Success is something you attract by the person you become.`,null, null,`Jim`, `Rohn`);
+newQuote(`Ordinary people believe only in the possible.`, `Extraordinary people visualize not what is possible or probably, but rather what is impossible.`, `And by visualizing the impossible, they begin to see it as possible.`, `Cherie`, `Carter-Scott`);
+newQuote(`Ideas can come from anywhere and at any time.`, `The problem with making mental notes is that the ink fades very rapidly.`, null, `Rolf`, `Smith`);
+newQuote(`If you really think about it, hitting the snooze button in the morning doesn't even make sense.`, `It's like saying, "I hate getting up in the morning, so I do it over, and over, and over again.`, null, `Demetri`, `Martin`);
+newQuote(`Reading is to the mind what exercise is to the body and prayer is to the soul.`, `We become the books we read.`, null, `Matthew`, `Kelly`);
+newQuote(`Successful people aren't born that way.`, `They become successful by establishing the habit of doing things unsuccessful people don't like to do.`, `The successful people don't always like doing these things themselves; they just get on and do them.`, `Don`, `Marquis`);
+newQuote(`One of the most common causes of failure is the habit of quitting when one is overtaken by temporary defeat.`, null, null, `Napoleon`, `Hill`);
+newQuote(`I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.`, null, null, `Bruce`, `Lee`);
+newQuote(`Whether you think you can or think you can't, you're right.`, null, null, `Henry`, `Ford`);
+newQuote(`Good is the enemy of great...`, `Few people attain great lives, in large part because it is just so easy to settle for a good life.`, `The vast majority of companies never become great, precisely because the vast majority become quite good - and that is their main problem.`, `Jim`, `Collins`);
+newQuote(`Winning is not a sometime thing: it's an all-the-time thing.`, `You don't win once in a while; you don't do the right thing once in a while; you do them right all the time.`, `Winning is a habit. Unfortunately, so is losing.`, `Vince`, `Lombardi`);
+newQuote(`No horse gets anywhere until he is harnessed.`, `No steam or gas ever drives anything until it is confined. No Niagara is ever turned into light and power until it is tunneled.`, `No life ever grows great until it is focused, dedicated, disciplined.`, `Harry`, `Emerson Fosdick`);
+*/
+/*
+console.log(newQuote);
+console.log(quotesArr);
+*/
+
